c1fc41c9cb9525c5e6215659e3590bf3e9175dcf
개미조 <nmnmnmlk6248@gmail.com>
2023-10-11 19:17:52 +0900

    [level unrated] Title: 뒤에서 5등 위로, Time: 0.54 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {};
+         Arrays.sort(num_list);
+         answer = Arrays.copyOfRange(num_list,5,num_list.length);
+    
+        return answer;
+    }
+}
