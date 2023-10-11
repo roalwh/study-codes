d0e94897c24beab92d9c835560254966c0874e51
개미조 <nmnmnmlk6248@gmail.com>
2023-10-11 19:23:24 +0900

    [level unrated] Title: 문자열 섞기, Time: 11.83 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String str1, String str2) {
+        String answer = "";
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        int i=0;
+        
+        for(i=0;i<arr1.length;i++){
+            answer += arr1[i];
+            answer += arr2[i];
+        }
+        
+        
+                
+        return answer;
+    }
+}
